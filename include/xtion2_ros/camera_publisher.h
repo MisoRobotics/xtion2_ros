c2351b17c5bd1930e13ebe8b0335d6d1ad60c90b
Ryan Sinnet <ryan@rwsinnet.com>
2018-02-10 16:51:29 -0800

    Adding publishing of CameraInfo.

--- include/xtion2_ros/camera_publisher.h
+++ include/xtion2_ros/camera_publisher.h
@@ -25,10 +25,10 @@ class IOInterface;
 class CameraPublisher
 {
   unsigned int counter_;
+  std::string camera_name_;
 
   ros::NodeHandle color_nh_;
   ros::NodeHandle depth_nh_;
-  ros::NodeHandle nh_;
   image_transport::ImageTransport color_it_;
   image_transport::ImageTransport depth_it_;
   camera_info_manager::CameraInfoManager color_manager_;
@@ -36,13 +36,20 @@ class CameraPublisher
   image_transport::Publisher color_pub_;
   image_transport::Publisher depth_pub_;
 
+  sensor_msgs::CameraInfo color_msg_;
+  sensor_msgs::CameraInfo depth_msg_;
+
+  ros::Publisher color_info_pub_;
+  ros::Publisher depth_info_pub_;
+
   void publishImage(const cv::Mat img, const std::string& type, image_transport::Publisher& publisher);
+  void publishInfo(sensor_msgs::CameraInfo& msg, ros::Publisher& publisher, const std::string& type);
 
   cv_bridge::CvImage img_bridge_;
 
 public:
   explicit CameraPublisher(const std::string& camera_name);
-
+  void initialize();
   void publish(IOInterface& xtion_interface);
 };
 }  // namespace xtion2_ros
--- src/camera_publisher.cxx
+++ src/camera_publisher.cxx
@@ -10,15 +10,32 @@
 namespace xtion2_ros
 {
 CameraPublisher::CameraPublisher(const std::string& camera_name)
-  : color_nh_("rgb")
-  , depth_nh_("depth")
+  : camera_name_(camera_name)
+  , color_nh_(camera_name + "/rgb")
+  , depth_nh_(camera_name + "/depth")
   , color_it_(color_nh_)
   , depth_it_(depth_nh_)
   , color_manager_(color_nh_)
   , depth_manager_(depth_nh_)
   , color_pub_(color_it_.advertise("image_raw", 1))
   , depth_pub_(depth_it_.advertise("image_raw", 1))
+  , color_info_pub_(color_nh_.advertise<sensor_msgs::CameraInfo>("camera_info", 1))
+  , depth_info_pub_(depth_nh_.advertise<sensor_msgs::CameraInfo>("camera_info", 1))
 {
+}
+
+void CameraPublisher::initialize()
+{
+  if (!color_manager_.setCameraName(camera_name_))
+  {
+    ROS_WARN_STREAM("[" << camera_name_ << "] name not valid"
+                        << " for camera_info_manger");
+  }
+  if (!depth_manager_.setCameraName(camera_name_))
+  {
+    ROS_WARN_STREAM("[" << camera_name_ << "] name not valid"
+                        << " for camera_info_manger");
+  }
 }
 
 void CameraPublisher::publishImage(const cv::Mat img, const std::string& type, image_transport::Publisher& publisher)
@@ -32,6 +49,21 @@ void CameraPublisher::publishImage(const cv::Mat img, const std::string& type, i
   publisher.publish(img_msg);
 }
 
+void CameraPublisher::publishInfo(sensor_msgs::CameraInfo& msg, ros::Publisher& publisher, const std::string& type)
+{
+  msg.header.seq = counter_;
+  msg.header.stamp = ros::Time::now();
+  msg.header.frame_id = camera_name_ + "/" + type + "/optical_frame";
+  msg.height = 240;
+  msg.width = 320;
+  msg.distortion_model = "plumb_bob";
+  msg.D = { { 0., 0., 0., 0., 0. } };
+  msg.K = { { 831.3820367867805, 0.0, 480.5, 0.0, 831.3820367867805, 270.5, 0.0, 0.0, 1.0 } };
+  msg.R = { { 0., 0., 0., 0., 0., 0., 0., 0., 0. } };
+  msg.P = { { 831.3820367867805, 0.0, 480.5, -0.0, 0.0, 831.3820367867805, 270.5, 0.0, 0.0, 0.0, 1.0, 0.0 } };
+  publisher.publish(msg);
+}
+
 void CameraPublisher::publish(IOInterface& xtion_interface)
 {
   if (!xtion_interface.framesValid())
@@ -42,11 +74,13 @@ void CameraPublisher::publish(IOInterface& xtion_interface)
   if (xtion_interface.color_new)
   {
     publishImage(xtion_interface.getColorFrame(), sensor_msgs::image_encodings::RGB8, color_pub_);
+    publishInfo(color_msg_, color_info_pub_, "rgb");
     xtion_interface.color_new = false;
   }
   if (xtion_interface.depth_new)
   {
     publishImage(xtion_interface.getDepthFrame(), sensor_msgs::image_encodings::TYPE_16UC1, depth_pub_);
+    publishInfo(depth_msg_, depth_info_pub_, "depth");
     xtion_interface.depth_new = false;
   }
 }
